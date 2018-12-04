d589f109610b83394d339e09ab3f74ecaf141b57
Xichen Sun <sunxichen@uqpay.com>
2018-12-04 10:42:48 +0800

    add uglifyjs

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const base = require('./webpack.config.base');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(base, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
 });
