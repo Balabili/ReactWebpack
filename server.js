852d0c145f6cd2d36e30a675f4f0fb24928197fe
Xichen Sun <sunxichen@uqpay.com>
2018-12-04 17:40:47 +0800

    test pre-commit

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const webpack = require('webpack');
 const opn = require('opn');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./config/webpack.config.dev');
 
 const app = express();
-const config = require('./config/webpack.config.dev');
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
-  opn(`http://localhost:3000`);
+app.listen(3000, () => {
+  opn('http://localhost:3000');
 });
